3d304355af1f4fc7c4061ea45df30216b00e47e9
Matt Dennewitz <mattdennewitz@gmail.com>
2014-10-04 17:59:38 -0500

    egregious bump to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-active-login-required',
-    version='0.1',
+    version='0.1.1',
     description='',
     author='Matt Dennewitz',
     author_email='mattdennewitz@gmail.com',
